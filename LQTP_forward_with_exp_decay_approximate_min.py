12ec7e2d1b20b6e3272ecee9e4217a67ace81d68
Lapo <lapo.faggi@unifi.it>
2022-04-22 17:55:07 +0200

    gradient descent to be finished

--- LQTP_forward_with_exp_decay_approximate_min.py
+++ LQTP_forward_with_exp_decay_approximate_min.py
@@ -6,9 +6,10 @@ import torch, torchvision
 from torchviz import make_dot
 
 T = 100
-lambda_decay = 0.1
+lambda_decay = 1.2
 
-a = 1
+
+a = -1
 b = 1
 q = 1
 r = 0.1
@@ -16,51 +17,68 @@ r = 0.1
 x_0 = 0
 p_0 = 0
 
-y = np.zeros(2)
-w = torch.tensor(1., requires_grad=True)
-x = torch.zeros(1, requires_grad=False)
-p = torch.zeros(1, requires_grad=False)
+class LQTP_problem:
+    def __init__(self,a,b,q,r,x_0,p_0,T,lambda_decay):
+        self.T = T
+        self.lambda_decay = lambda_decay
 
-optim = torch.optim.Adam([w], lr=0.001)
-n_int = 10
+        self.a = a
+        self.b = b
+        self.q = q
+        self.r = r
 
+        self.x_0 = x_0
+        self.p_0 = p_0
 
-def signal(t):
-    return np.array(math.sin(t)) # define the signal to be tracked
+        self.w = torch.tensor(0.1,requires_grad=True)
+        self.x = torch.tensor(0.1, requires_grad=False)
+        self.p = torch.tensor(0.1, requires_grad=False)
 
-def sigma(w):
-    return math.tanh(w) # define your own function
+        self.n_int = 100
 
-def hamiltonian(w,x,p,t):
-    h = torch.tensor(0.5 * q * math.exp(-lambda_decay*(T-t)) * (x-signal(t))**2 + 0.5 * r * w**2 + p * (a * x + b * sigma(w)))
-    print(h)
-    return h
+        self.y = np.zeros(2)
 
-def eval_w_min(w,x,p,t):
-    t_tensor = torch.tensor(t,requires_grad=False)
-    # TODO trasforma y[0] y[1] in tensori torch x p
-    for i in range(n_int):
-        loss = hamiltonian(w,x,p,t_tensor)
-        make_dot(loss).render("grafico", format="png")
-        print(loss)
-        print(w)
-        loss.backward()
-        optim.step()
-        w.zero_grad()
-        exit()
-    return w.detach().cpu().numpy()
+    def signal(self,t):
+        return np.array(math.sin(t)) # define the signal to be tracked
 
-def fun(t, y):
-    return [a*y[0]+ b * sigma(eval_w_min(w,y[0],y[1],t)), -q*math.exp(-lambda_decay*(T-t))*(y[0] - signal(t))-a*y[1]]
+    def sigma(self,w):
+        # return w
+        return math.tanh(w) # define your own function
 
+    def hamiltonian(self,w,x,p,t):
+        h = torch.tensor(0.5 * self.q * math.exp(-self.lambda_decay*(self.T-t)) * (x-self.signal(t))**2 + 0.5 * self.r * w**2 + p * (self.a * x + self.b * self.sigma(w)))
+        print(h)
+        return h
 
-sol = solve_ivp(fun, [0,T], [x_0,p_0], dense_output=True, rtol=10**-10, method="DOP853")
+    def eval_w_min(self, x, p, t):
+        t_tensor = torch.tensor(t, requires_grad=False)
+        self.x.data = torch.from_numpy(x)
+        self.p.data = torch.from_numpy(p)
+        for i in range(self.n_int):
+            loss = self.hamiltonian(self.w, x, p, t_tensor)
+            make_dot(loss).render("grafico", format="png")
+            print(loss)
+            print(self.w)
+            loss.backward()
+            self.optim.step()
+            self.w.zero_grad()
+        return self.w.detach().cpu().numpy()
+
+    def fun(self,t, y):
+        return [self.a*y[0]+ self.b * self.sigma(self.eval_w_min(y[0],y[1],t)), -self.q*math.exp(-self.lambda_decay*(self.T-t))*(y[0] - self.signal(t))-self.a*y[1]]
+
+    def solve_problem(self):
+        return solve_ivp(self.fun, [0,self.T], [self.x_0,self.p_0], dense_output=True, rtol=10**-10, method="DOP853")
+
+problem = LQTP_problem(a,b,q,r,x_0,p_0,T,lambda_decay)
+
+sol = problem.solve_problem()
 
 n_samples = 1000
 t_plot = np.linspace(0, T, n_samples)
 signal_plot=[]
 for i in range(0,n_samples):
-    signal_plot.append(signal(t_plot[i]))
+    signal_plot.append(problem.signal(t_plot[i]))
 
 
 plt.plot(t_plot, sol.sol(t_plot)[0], label='State',color="blue")
