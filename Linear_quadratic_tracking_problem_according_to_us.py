b0dbedac9b3d9f7c2b2af0d2b3d81d2e07c71c0e
Lapo <lapo.faggi@unifi.it>
2022-09-15 11:52:00 +0200

    linear_quadratic_tracking_problem_according_to_us working

--- Linear_quadratic_tracking_problem_according_to_us.py
+++ Linear_quadratic_tracking_problem_according_to_us.py
@@ -3,35 +3,42 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
+from scipy import signal
 
 n = 20000
 T = 250
-window_size = 3
+window_size = 5
 
 a = 1
 b = 1
-r = 1
+
 q = 1
+r = 0.001
 
 t_array = torch.linspace(0, T, n)
 dt = T / n
 
-threshold = 6
+threshold = 100
 
 
-def input_fun(t):
+def input_fun(t):     # Sinusoidal function
     return 5 * torch.sin(2 * np.pi * 0.01 * t)
+
+# def input_fun(t):       # Squared-wave function
+#     return torch.tensor(float(signal.square(2*np.pi*0.01*t)))
 
 class NeuralModel(torch.nn.Module):
     def __init__(self):
         super(NeuralModel, self).__init__()
         self.hidden = 100
-        # self.activation = torch.nn.ReLU()
+        self.activation = torch.nn.ReLU()
         self.activation = torch.nn.Tanh()
         self.linear_layer1 = torch.nn.Linear(2, self.hidden)
         self.linear_layer2 = torch.nn.Linear(self.hidden, self.hidden)
         self.linear_layer3 = torch.nn.Linear(self.hidden, self.hidden)
         self.linear_layer4 = torch.nn.Linear(self.hidden, 1)
+
+        # Original proposal
         # self.theta = torch.nn.parameter.Parameter(0*torch.ones(1))
 
     def forward(self, x):
@@ -67,7 +74,7 @@ def backward_learning(x0,p0,t_index,net, window):
     x_train[-1] = x0
 
     # create dataset of optimal solutions going backward
-    for window_index in range(window-1):   # TODO da ricontrollare indici segnale input, ricontrollare segni
+    for window_index in range(window-1):   # TODO da ricontrollare indici segnale input, ricontrollare segni membro destra
         x_train[window-window_index-2] = x_train[window-window_index-1] - dt * (a * x_train[window-window_index-1] - (b ** 2 / r) * p_train[window-window_index-1])
         p_train[window - window_index - 2] = p_train[window - window_index-1] - dt * (-q * (x_train[window - window_index - 1] - input_fun(torch.tensor((t_index - window_index) * dt))) - a * p_train[window - window_index - 1])
 
@@ -75,12 +82,12 @@ def backward_learning(x0,p0,t_index,net, window):
         signal_train[window-window_index-1] = input_fun(torch.tensor((t_index - window_index) * dt))
 
     # Marco's conditions
-    for window_index in range(window-1):
-        if abs(x_train[window-window_index-1]) > 100 or abs(p_train[window-window_index-1]) > 100 or abs((x_train[window-window_index-1] - signal_train[window-window_index-1])) < 0.0001:
-            x_train = x_train[window-window_index:-1]
-            p_train = p_train[window - window_index:-1]
-            signal_train = signal_train[window - window_index:-1]
-            break
+    # for window_index in range(window-1):
+    #     if abs(x_train[window-window_index-1]) > 50 or abs(p_train[window-window_index-1]) > 50 or abs((x_train[window-window_index-1] - signal_train[window-window_index-1])) < 0.0001:
+    #         x_train = x_train[window-window_index:-1]
+    #         p_train = p_train[window - window_index:-1]
+    #         signal_train = signal_train[window - window_index:-1]
+    #         break
 
     batch = torch.stack((x_train,signal_train),dim = 1)
     labels = p_train.unsqueeze(dim=1)
@@ -100,8 +107,8 @@ def update_model(net, batch, labels):
 if __name__ == "__main__":
 
     # xf = torch.zeros(n)
-    xf = 0.1*torch.ones(n)
-    pf = torch.zeros(n)
+    xf = 1 * torch.ones(n)
+    pf = 0.5 * torch.ones(n) # Indifferente, viene sovrascritto dalla stima del modello nel forward
 
     theta_for_plot = torch.ones(n)
 
@@ -109,12 +116,23 @@ if __name__ == "__main__":
     criterion = nn.MSELoss(reduction='sum')
     optimizer = optim.SGD(model.parameters(), lr=0.001)
 
+    weights_norm_array = torch.zeros(len(t_array))
+
     # for par in model.parameters():
     #     print(par.size())
 
     for t in range(len(t_array)-1):
 
         backward_learning(xf[t], 0, t, model, window_size)
+
+        # Average Weights' L2 norm for debugging purposes
+        total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+        for par in model.parameters():
+            if par.requires_grad:
+                weights_norm_array[t] += torch.sqrt(torch.sum(par**2))
+        weights_norm_array[t] /= total_params
+
         forward_step(xf, pf, t, model)
 
         # for par in model.parameters():
@@ -122,36 +140,48 @@ if __name__ == "__main__":
 
         # Reset if x or p is out of the bounding box
         if abs(xf[t+1])>threshold or abs(pf[t])>threshold:
+            print("Reset!")
             xf[t+1] = 0
-            pf[t] = 0
+            # pf[t] = 0  # Inutile, tanto nel forward
+
         if t % 1000 == 0:
             print(f"Iteration {t}/{n}")
 
-        # forward_step(xf, pf, t, model)
-        #
-        # if abs(xf[t+1]- input_fun(torch.tensor((t+1)*dt))) > 0.0001 or abs(xf[t+1])<threshold or abs(pf[t])<threshold:  # definire condizione per cui avviene learning
-        #     backward_learning(xf[t], 0 , t, model, window_size)
-        #
-        # for par in model.parameters():
-        #     theta[t] = par.data
-        #
-        # # Reset if x or p is out of the bounding box
-        # if abs(xf[t+1])>threshold or abs(pf[t])>threshold:
-        #     xf[t+1] = 0
-        #     pf[t] = 0
+        # Last step:
+        with torch.no_grad():
+            input = torch.tensor((xf[-1], input_fun(torch.tensor(T)))).unsqueeze(0)
+            pf[-1] = model(input)
+
+        for par in model.parameters():
+            if par.requires_grad:
+                weights_norm_array[-1] += torch.sqrt(torch.sum(par**2))
+        weights_norm_array[-1] /= total_params
+
 
     plt.figure(0)
     plt.plot(t_array,xf, label="State",color="green")
     plt.plot(t_array, pf, label="Costate", color="red")
-    plt.plot(t_array,input_fun(t_array), label="Signal", color="cyan")
+
+    signal_array = torch.zeros(len(t_array))
+    for t_index in range(len(t_array)):
+        signal_array[t_index] = input_fun(torch.tensor(t_index * dt))
+
+    plt.plot(t_array, signal_array, label="Signal", color="cyan")
     plt.ylim((-10,10))
 
     plt.legend()
+
 
     # plt.figure(1)
     # plt.plot(t_array, theta_for_plot, label="Theta", color="orange")
     # plt.ylim((-5,5))
     # plt.legend()
+
+    print(weights_norm_array.detach().numpy())
+
+    plt.figure(1)
+    plt.plot(t_array, weights_norm_array.detach().numpy(), label="Weights norm", color="orange")
+    plt.legend()
 
     plt.show()
 
